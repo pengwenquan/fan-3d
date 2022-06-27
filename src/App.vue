c4c857e5fcefba870be177302417a7341247757a
pengwenquan <pengwenquan@kp99.cn>
2022-06-27 20:33:17 +0800

    feat: 初始化3d场景，添加风机以及地形

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <Fanji></Fanji>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Fanji from "./components/fan-ji.vue";
+
+export default defineComponent({
+  components: {
+    Fanji,
+  },
+});
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  width: 100vw;
+  height: 100vh;
+  background: url("./assets/bg.png") center center no-repeat;
 }
 </style>
---
+++ src/components/fan-ji.vue
@@ -0,0 +1,270 @@
+<template>
+  <div id="container"></div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { TweenMax, Linear } from "gsap";
+
+let scene = new THREE.Scene();
+let loader = new GLTFLoader();
+let arrTweenAn = [];
+let fan = new THREE.Object3D();
+let land1, land2, land3;
+let sceneOne = new THREE.Object3D();
+
+function init() {
+  let camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    10000
+  );
+  camera.position.set(10, 7, 20);
+  scene.add(camera);
+
+  let light = new THREE.DirectionalLight(0xffffff);
+  light.position.set(20, 20, 20);
+  scene.add(light);
+
+  let renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true,
+    logarithmicDepthBuffer: true,
+  });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(2);
+
+  document.getElementById("container")?.appendChild(renderer.domElement);
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.maxDistance = 1000;
+  controls.minPolarAngle = 0;
+  controls.maxPolarAngle = Math.PI / 2;
+
+  function tick() {
+    controls.update();
+    renderer.render(scene, camera);
+    requestAnimationFrame(tick);
+  }
+  tick();
+}
+
+function cloneFan(x, y, z) {
+  let fan_clone = fan.clone();
+  fan_clone.position.set(x, y, z);
+  TweenMax.to(fan_clone.children[0].rotation, 5, {
+    x: fan_clone.children[0].rotation.x + Math.PI * 2,
+    repeat: -1,
+    ease: Linear.easeOut,
+  });
+  sceneOne.add(fan_clone);
+}
+
+function cloneLand1(x, y, z) {
+  let land1_clone = land1.clone();
+  land1_clone.position.set(x, y, z);
+  sceneOne.add(land1_clone);
+}
+
+function cloneLand2(x, y, z) {
+  let land2_clone = land2.clone();
+  land2_clone.position.set(x, y, z);
+  sceneOne.add(land2_clone);
+}
+
+function addModel() {
+  //加载叶片模型
+  let p1 = new Promise((resolve) => {
+    loader
+      .setPath("./model/")
+      .setDRACOLoader(new DRACOLoader().setDecoderPath("./js/draco/gltf/"))
+      .load("GLTF/yepian.glb", (gltf) => {
+        TweenMax.to(gltf.scene.rotation, 5, {
+          x: gltf.scene.rotation.x + Math.PI * 2,
+          repeat: -1,
+          ease: Linear.easeOut,
+        });
+
+        // arrTweenAn.push(an);
+        gltf.scene.position.set(-2.22, 99.65, 0);
+        resolve(gltf.scene);
+      });
+  });
+
+  // 加载风机柱子
+  let p2 = new Promise((resolve) => {
+    loader
+      .setPath("./model/")
+      .setDRACOLoader(new DRACOLoader().setDecoderPath("./js/draco/gltf/"))
+      .load("GLTF/zhuji2.glb", (gltf) => {
+        gltf.scene.position.set(17.3, 2.73, -1.85);
+        resolve(gltf.scene);
+      });
+  });
+
+  // 加载地形1
+  let p3 = new Promise((resolve) => {
+    loader
+      .setPath("./model/")
+      .setDRACOLoader(new DRACOLoader().setDecoderPath("./js/draco/gltf/"))
+      .load("GLTF/dixing1.glb", (gltf) => {
+        land1 = gltf.scene;
+        gltf.scene.traverse((child) => {
+          if (child.isMesh) {
+            child.material.side = 2;
+            if (child.name.includes("地形") || child.name.includes("山脉")) {
+              child.material.transparent = true;
+              child.material.metalness = 0.8;
+              child.material.roughness = 0.71;
+              child.material.opacity = 1;
+              child.material.color = new THREE.Color(0x132441);
+              child.material.emissive = new THREE.Color(0x132441);
+            }
+          }
+        });
+        resolve(gltf.scene);
+      });
+  });
+
+  // 加载地形2
+  let p4 = new Promise((resolve) => {
+    loader
+      .setPath("./model/")
+      .setDRACOLoader(new DRACOLoader().setDecoderPath("./js/draco/gltf/"))
+      .load("GLTF/dixing2.glb", (gltf) => {
+        land2 = gltf.scene;
+        gltf.scene.traverse((child) => {
+          if (child.isMesh) {
+            child.material.side = 2;
+            if (child.name.includes("地形") || child.name.includes("山脉")) {
+              child.material.transparent = true;
+              child.material.metalness = 0.8;
+              child.material.roughness = 0.71;
+              child.material.opacity = 1;
+              child.material.color = new THREE.Color(0x132441);
+              child.material.emissive = new THREE.Color(0x132441);
+            }
+          }
+        });
+        resolve(gltf.scene);
+      });
+  });
+
+  // 加载地形3
+  let p5 = new Promise((resolve) => {
+    loader
+      .setPath("./model/")
+      .setDRACOLoader(new DRACOLoader().setDecoderPath("./js/draco/gltf/"))
+      .load("GLTF/dixing3.glb", (gltf) => {
+        land3 = gltf.scene;
+        gltf.scene.traverse((child) => {
+          if (child.isMesh) {
+            child.material.side = 2;
+            if (child.name.includes("地形") || child.name.includes("山脉")) {
+              child.material.transparent = true;
+              child.material.metalness = 0.8;
+              child.material.roughness = 0.71;
+              child.material.opacity = 1;
+              child.material.color = new THREE.Color(0x132441);
+              child.material.emissive = new THREE.Color(0x132441);
+            }
+          }
+        });
+        resolve(gltf.scene);
+      });
+  });
+  Promise.all([p1, p2, p3, p4, p5]).then((result) => {
+    fan.add(result[0], result[1]);
+    fan.scale.set(0.005, 0.005, 0.005);
+    fan.position.set(5.11, 3.19598, 12.016);
+    fan.rotation.set(0, 89.5, 0);
+
+    // 克隆风机
+    cloneFan(0.74852, 2.56534, 7.44558);
+    cloneFan(8.29076, 3.19363, 13.22294);
+    cloneFan(5.24642, 3.49002, 10.11123);
+    cloneFan(7.75871, 3.77725, 10.01902);
+    cloneFan(-0.04669, 2.94419, 3.38853);
+    cloneFan(6.58057, 3.38455, 0.83203);
+    cloneFan(10.53541, 3.58002, -2.88202);
+    cloneFan(-3.43544, 2.85505, 5.41173);
+    cloneFan(-2.74084, 2.92708, 11.78238);
+    cloneFan(-10.1402, 3.29675, 2.79538);
+    cloneFan(-2.99843, 3.09172, -6.22022);
+    cloneFan(15.37429, 3.50198, -0.2355);
+    cloneFan(9.12926, 3.19598, -14.55305);
+    cloneFan(3.46791, 2.56157, 0.53953);
+
+    // 克隆地形1
+    cloneLand1(8.70549, 2.86865, -1.35901);
+    cloneLand1(18.70101, 2.64197, 10.13471);
+    cloneLand1(-5.72901, 2.8955, 15.39313);
+    cloneLand1(21.694, 2.56808, 18.92285);
+    cloneLand1(7.39948, 2.72258, -13.35363);
+    cloneLand1(14.23939, 2.75916, -27.02723);
+    cloneLand1(17.33572, 2.73076, -1.85427);
+    cloneLand1(17.33572, 2.73076, -14.2357);
+    cloneLand1(-10.18862, 2.90444, -18.9965);
+    cloneLand1(-15.615, 2.73076, -28.86956);
+    cloneLand1(-15.615, 2.73076, -9.47457);
+    cloneLand1(5.3932, 2.84328, -33.30284);
+    cloneLand1(9.75895, 2.73076, -46.883);
+    cloneLand1(17.33572, 2.73076, -37.66992);
+    cloneLand1(28.728, 2.89401, -26.29959);
+    cloneLand1(28.7608, 2.781, -1.85427);
+    cloneLand1(28.702, 2.73076, 18.6807);
+    cloneLand1(28.702, 2.73076, 33.284);
+    cloneLand1(11.26473, 2.8502, 33.284);
+    cloneLand1(-5.63942, 2.73076, 23.43441);
+    cloneLand1(-15.615, 2.94279, 33.284);
+    cloneLand1(-15.615, 2.73076, 13.04465);
+    cloneLand1(-15.615, 2.94475, 0.39202);
+    cloneLand1(-15.615, 2.73076, -19.4563);
+    cloneLand1(28.702, 2.73076, -46.883);
+    cloneLand1(-15.615, 2.73076, -46.883);
+
+    // 克隆地形2
+    cloneLand2(-4.39732, 2.31106, -7.5534);
+    cloneLand2(-11.34043, 2.31106, 3.62787);
+    cloneLand2(7.58834, 2.31106, 24.15814);
+    cloneLand2(-0.14082, 2.31106, -18.75615);
+    cloneLand2(-5.01234, 2.31106, -32.15209);
+    cloneLand2(-4.23729, 2.0619, -46.883);
+    cloneLand2(21.94042, 2.31106, -46.88289);
+    cloneLand2(28.702, 2.31106, -36.6848);
+    cloneLand2(28.68036, 2.28686, -13.23737);
+    cloneLand2(28.716, 2.26106, 8.15521);
+    cloneLand2(18.61536, 2.31106, 33.284);
+    cloneLand2(-0.92105, 2.31106, 33.284);
+    cloneLand2(-15.615, 2.31106, 22.367);
+    cloneLand2(-15.615, 2.14719, -38.39289);
+    scene.add(fan);
+    scene.add(land1);
+    scene.add(land2);
+    scene.add(land3);
+    land1.position.set(17.33572, 2.7305, -1.85427);
+    land2.position.set(20.758, 1.73076, -3.13564);
+    land3.position.set(6.37, 2.578, 11.246);
+    land3.rotation.set(0, 29.87, 0);
+    scene.add(sceneOne);
+  });
+}
+
+onMounted(() => {
+  init();
+  addModel();
+});
+</script>
+
+<style scoped>
+#container {
+  width: 100%;
+  height: 100%;
+}
+</style>
